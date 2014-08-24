107fcaf97e02c503dff2abe4eaf7baa4a755a384
woalk <private@woalk.de>
2014-08-24 12:55:44 +0200

    Moved MySQL-HttpPost-Things from MyAppsActivity
    This also corrected a bug, when there are apps in the database that are not currently installed on the device.

--- src/com/woalk/apps/xposed/ttsb/community/MyAppsActivity.java
+++ src/com/woalk/apps/xposed/ttsb/community/MyAppsActivity.java
@@ -1,27 +1,14 @@
 package com.woalk.apps.xposed.ttsb.community;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.woalk.apps.xposed.ttsb.*;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -29,10 +16,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -41,7 +26,11 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+
+import com.woalk.apps.xposed.ttsb.Helpers;
+import com.woalk.apps.xposed.ttsb.R;
+import com.woalk.apps.xposed.ttsb.Settings;
+import com.woalk.apps.xposed.ttsb.community.SQL_Operations.Q;
 
 public class MyAppsActivity extends Activity {
 	public static final String PASS_ALL_APPS = Helpers.TTSB_PACKAGE_NAME
@@ -83,137 +72,119 @@ public class MyAppsActivity extends Activity {
 		getSyncables();
 	}
 
+	private AlertDialog progress;
+
 	protected void getSyncables() {
-		new readDatabaseTask().execute(Database.DATABASE_URL);
-	}
+		Q q = new Q(Database.DATABASE_URL);
+		q.setPreExecuteListener(new Q.PreExecuteListener() {
 
-	private class readDatabaseTask extends
-			AsyncTask<String, String, MyAppsAdapter> {
-		private AlertDialog progress;
-
-		@Override
-		protected void onPreExecute() {
-			AlertDialog.Builder builder = new AlertDialog.Builder(
-					MyAppsActivity.this);
-			builder.setMessage(R.string.loadingsync_msg);
-			builder.setView(new ProgressBar(MyAppsActivity.this));
-			progress = builder.create();
-			progress.show();
-		}
-
-		@Override
-		protected MyAppsAdapter doInBackground(String... params) {
-			if (all_apps_p == null) {
-				all_apps_p = new TreeMap<String, ApplicationInfo>();
-				for (ApplicationInfo app : all_apps) {
-					all_apps_p.put(app.packageName, app);
-				}
-			}
-
-			List<ApplicationInfo> apps = new ArrayList<ApplicationInfo>();
-			List<String> packageNames = new ArrayList<String>();
-			List<Boolean> is_set = new ArrayList<Boolean>();
-			List<Boolean> has_update = new ArrayList<Boolean>();
-
-			String result = "";
-			try {
-				// get database connection & package entries
-				HttpClient httpclient = new DefaultHttpClient();
-				HttpPost httppost = new HttpPost(params[0]);
-				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-				nameValuePairs.add(new BasicNameValuePair(Database.POST_PIN,
-						Database.COMMUNITY_PIN));
-				nameValuePairs
-						.add(new BasicNameValuePair(Database.POST_FUNCTION,
-								Database.FUNCTION_GET_PACKAGES));
-				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-				HttpResponse response = httpclient.execute(httppost);
-				HttpEntity entity = response.getEntity();
-
-				InputStream is = entity.getContent();
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(is, "utf-8"), 8);
-				StringBuilder sb = new StringBuilder();
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					sb.append(line + "\n");
-				}
-				is.close();
-				result = sb.toString();
-				JSONArray jArray = new JSONArray(result);
-				for (int i = 0; i < jArray.length(); i++) {
-					JSONObject json_data = jArray.getJSONObject(i);
-					String packageName = json_data.getString("package");
-					if (packageNames.contains(packageName))
-						continue;
-					packageNames.add(packageName);
-					apps.add(all_apps_p.get(packageName));
-					boolean is_current_set = Settings.Loader.containsPackage(
-							getApplicationContext(), packageName);
-					boolean has_current_update = false;
-					if (is_current_set) {
-						SharedPreferences communityPref = getApplicationContext()
-								.getSharedPreferences(
-										Database.Preferences.COMMUNITY_PREF_NAME,
-										Context.MODE_PRIVATE);
-						if (communityPref
-								.getBoolean(
-										Database.Preferences.PREF_PREFIX_IS_TOPVOTED_USED
-												+ packageName, false)) {
-							int topvoted_id = communityPref
-									.getInt(Database.Preferences.PREF_PREFIX_USED_SUBMIT_ID
-											+ packageName, -1);
-							has_current_update = topvoted_id != Integer
-									.valueOf(json_data.getString("id"))
-									&& topvoted_id != -1;
-						}
-					}
-					is_set.add(is_current_set);
-					has_update.add(has_current_update);
-				}
-			} catch (UnknownHostException e) {
-				publishProgress("404");
-				return null;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return null;
-			}
-
-			return new MyAppsAdapter(MyAppsActivity.this, apps, is_set,
-					has_update);
-		}
-
-		@Override
-		protected void onProgressUpdate(String... progress) {
-			if (progress[0] == "404")
-				Toast.makeText(MyAppsActivity.this, R.string.no_connection_e,
-						Toast.LENGTH_LONG).show();
-			else {
+			@Override
+			public void onPreExecute() {
 				AlertDialog.Builder builder = new AlertDialog.Builder(
 						MyAppsActivity.this);
-				builder.setMessage(Html.fromHtml(progress[0]));
-				builder.show();
+				builder.setMessage(R.string.loadingsync_msg);
+				builder.setView(new ProgressBar(MyAppsActivity.this));
+				progress = builder.create();
+				progress.show();
 			}
-		}
+		});
+		final String key_apps = "apps";
+		final String key_packageNames = "pkgs";
+		final String key_is_set = "isset";
+		final String key_has_update = "update";
+		q.setDataLoadedListener(new Q.DataLoadedListener() {
 
-		@Override
-		protected void onPostExecute(MyAppsAdapter result) {
-			if (result != null) {
-				lA.apps.clear();
-				lA.is_set.clear();
-				lA.has_update.clear();
+			@Override
+			public Bundle onDataLoaded(JSONArray data) throws JSONException {
+				if (all_apps_p == null) {
+					all_apps_p = new TreeMap<String, ApplicationInfo>();
+					for (ApplicationInfo app : all_apps) {
+						all_apps_p.put(app.packageName, app);
+					}
+				}
 
-				lA.apps.addAll(result.apps);
-				lA.is_set.addAll(result.is_set);
-				lA.has_update.addAll(result.has_update);
+				ArrayList<ApplicationInfo> apps = new ArrayList<ApplicationInfo>();
+				ArrayList<String> packageNames = new ArrayList<String>();
+				ArrayList<Integer> is_set = new ArrayList<Integer>();
+				ArrayList<Integer> has_update = new ArrayList<Integer>();
 
-				lA.notifyDataSetChanged();
+				try {
+					for (int i = 0; i < data.length(); i++) {
+						JSONObject json_data = data.getJSONObject(i);
+						String packageName = json_data.getString("package");
+						if (packageNames.contains(packageName)
+								|| !all_apps_p.containsKey(packageName))
+							continue;
+						packageNames.add(packageName);
+						apps.add(all_apps_p.get(packageName));
+						boolean is_current_set = Settings.Loader
+								.containsPackage(getApplicationContext(),
+										packageName);
+						boolean has_current_update = false;
+						if (is_current_set) {
+							SharedPreferences communityPref = getApplicationContext()
+									.getSharedPreferences(
+											Database.Preferences.COMMUNITY_PREF_NAME,
+											Context.MODE_PRIVATE);
+							if (communityPref
+									.getBoolean(
+											Database.Preferences.PREF_PREFIX_IS_TOPVOTED_USED
+													+ packageName, false)) {
+								int topvoted_id = communityPref
+										.getInt(Database.Preferences.PREF_PREFIX_USED_SUBMIT_ID
+												+ packageName, -1);
+								has_current_update = topvoted_id != Integer
+										.valueOf(json_data.getString("id"))
+										&& topvoted_id != -1;
+							}
+						}
+						is_set.add(is_current_set ? 1 : 0);
+						has_update.add(has_current_update ? 1 : 0);
+					}
 
-				lv.setFastScrollEnabled(true);
+					Bundle bundle = new Bundle();
+					bundle.putParcelableArrayList(key_apps, apps);
+					bundle.putStringArrayList(key_packageNames, packageNames);
+					bundle.putIntegerArrayList(key_is_set, is_set);
+					bundle.putIntegerArrayList(key_has_update, has_update);
+					return bundle;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return null;
+				}
 			}
-			progress.dismiss();
-		}
+		});
+		q.setPostExecuteListener(new Q.PostExecuteListener() {
 
+			@Override
+			public void onPostExecute(Bundle processed) {
+				if (processed != null) {
+					lA.apps.clear();
+					lA.is_set.clear();
+					lA.has_update.clear();
+
+					ArrayList<ApplicationInfo> apps = processed
+							.getParcelableArrayList(key_apps);
+					lA.apps.addAll(apps);
+					for (Integer i : processed.getIntegerArrayList(key_is_set)) {
+						lA.is_set.add(i == 1);
+					}
+					for (Integer i : processed
+							.getIntegerArrayList(key_has_update)) {
+						lA.has_update.add(i == 1);
+					}
+
+					lA.notifyDataSetChanged();
+
+					lv.setFastScrollEnabled(true);
+				}
+				progress.dismiss();
+			}
+		});
+		q.addNameValuePair(Database.POST_PIN, Database.COMMUNITY_PIN);
+		q.addNameValuePair(Database.POST_FUNCTION,
+				Database.FUNCTION_GET_PACKAGES);
+		q.exec();
 	}
 
 	protected void openAppDetails(ApplicationInfo app) {
